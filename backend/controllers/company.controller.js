c7aa86a65f03f80f54d386f9989b5404ee42b1a8
anantdeepsingh <anantdeep2004@gmail.com>
2024-12-16 17:01:44 +0530

    company controller and routes added

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -32,12 +32,95 @@ export const registerCompany = async (req, res) => {
         });
 
 
-        
+
         return res.status(201).json({
             message: "Company registered successfully.",
+            company,
             success: true
         });
 
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+// get all the companies that are been registered
+export const getCompany = async (req, res) => {
+    try {
+        const userId = req.id; // logged in user id
+        const companies = await Company.find({ userId });
+        if (!companies) {
+            return res.status(404).json({
+                message: "No company found for given user Id.",
+                success: false
+            });
+        }
+
+        return res.status(200).json({
+            message: "Company Found",
+            companies,
+            success: true
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+//  get company by id
+
+
+export const getCompanyById = async (req, res) => {
+    try {
+        // url pass karte hai toh id eise pass hoti hai :/ params use karenge
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if (!company) {
+            return res.status(400).json({
+                message: "No such company found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            message: "Company found with given id.",
+            company,
+            success: true
+        });
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+export const updateCompany = async (req, res) => {
+    try {
+        const { name, description, website, location } = req.body;
+        const file = req.file;
+        // cloudinary 
+
+
+        const updateData = { name, description, website, location };
+
+        // finding company by id and update the data by using new true means data will be updated
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
+
+
+        // if company not found
+
+
+        if (!company) {
+            return res.status(404).json({
+                message: "No such company found",
+                success: false
+            });
+        }
+        return res.status(200).json({
+            message: "Data updated successfully.",
+            success: true
+        });
     }
     catch (error) {
         console.log(error);
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 const app=express();
 
@@ -27,6 +28,7 @@ const PORT=process.env.PORT || 3000;
 //  apis's
 
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",companyRoute);
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
