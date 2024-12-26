2ab0ad84dfe3396348c6ce3999ef8c255853145e
Anant <anantpandey175@gmail.com>
2024-12-26 17:16:29 +0530

    Browse page done

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Navbar from './shared/Navbar'
+import Job from './Job';
+
+const randomJobs = [1, 2, 3, 9, 8,7];
 
 export default function Browse() {
   return (
-    <div>Browse</div>
+    <div>
+        <Navbar/>
+        <div className='max-w-7xl mx-auto my-10'>
+            <h1 className='font-bold text-xl my-10'>Search Results ({randomJobs.length})</h1>
+            <div className='grid grid-cols-3 gap-4 mt-5'>
+            {
+                randomJobs.map((el, ind) => {
+                    return(
+                        <Job/>
+                    )
+                })
+            }
+            </div>
+        </div>
+    </div>
   )
 }
