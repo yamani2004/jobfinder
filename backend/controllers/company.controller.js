caa1a9c6dd9cf44655e54082760f15baa1fe471d
anantdeepsingh <anantdeep2004@gmail.com>
2024-12-28 19:45:54 +0530

    company controller and user controller modified

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,128 +1,100 @@
 import { Company } from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
+
 export const registerCompany = async (req, res) => {
     try {
-        // taking company name
         const { companyName } = req.body;
-
-        // if company name not found 
         if (!companyName) {
             return res.status(400).json({
                 message: "Company name is required.",
                 success: false
-            })
+            });
         }
-
-        // finding the company by comparing its schema created in model
         let company = await Company.findOne({ name: companyName });
-
-
-        // company name should be unique 
         if (company) {
             return res.status(400).json({
-                message: "Company name already exist.",
+                message: "You can't register same company.",
                 success: false
             })
         };
-
-        //  creating company 
-
         company = await Company.create({
             name: companyName,
-            userId: req.id // can be received from authentication 
+            userId: req.id
         });
-
-
 
         return res.status(201).json({
             message: "Company registered successfully.",
             company,
             success: true
-        });
-
-    }
-    catch (error) {
+        })
+    } catch (error) {
         console.log(error);
     }
 }
-// get all the companies that are been registered
 export const getCompany = async (req, res) => {
     try {
         const userId = req.id; // logged in user id
         const companies = await Company.find({ userId });
         if (!companies) {
             return res.status(404).json({
-                message: "No company found for given user Id.",
-                success: false
-            });
-        }
-
-        return res.status(200).json({
-            message: "Company Found",
-            companies,
-            success: true
-        });
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-//  get company by id
-
-
-export const getCompanyById = async (req, res) => {
-    try {
-        // url pass karte hai toh id eise pass hoti hai :/ params use karenge
-        const companyId = req.params.id;
-        const company = await Company.findById(companyId);
-        if (!company) {
-            return res.status(400).json({
-                message: "No such company found.",
+                message: "Companies not found.",
                 success: false
             })
         }
         return res.status(200).json({
-            message: "Company found with given id.",
-            company,
-            success: true
-        });
-
-    }
-    catch (error) {
+            companies,
+            success:true
+        })
+    } catch (error) {
         console.log(error);
     }
 }
-
-
-
+// get company by id
+export const getCompanyById = async (req, res) => {
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if (!company) {
+            return res.status(404).json({
+                message: "Company not found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            company,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 export const updateCompany = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
+ 
         const file = req.file;
-        // cloudinary 
+        // idhar cloudinary ayega
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
+    
+        const updateData = { name, description, website, location, logo };
 
-
-        const updateData = { name, description, website, location };
-
-        // finding company by id and update the data by using new true means data will be updated
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
-
-
-        // if company not found
-
 
         if (!company) {
             return res.status(404).json({
-                message: "No such company found",
+                message: "Company not found.",
                 success: false
-            });
+            })
         }
         return res.status(200).json({
-            message: "Data updated successfully.",
-            success: true
-        });
-    }
-    catch (error) {
+            message:"Company information updated.",
+            success:true
+        })
+
+    } catch (error) {
         console.log(error);
     }
 }
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,16 +3,17 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
+
 export const register = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
+         
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
                 message: "Something is missing",
                 success: false
             });
         };
-
         const file = req.file;
         const fileUri = getDataUri(file);
         const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
@@ -20,11 +21,12 @@ export const register = async (req, res) => {
         const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({
-                message: "User already exist with same mail Id",
+                message: 'User already exist with this email.',
                 success: false,
             })
         }
         const hashedPassword = await bcrypt.hash(password, 10);
+
         await User.create({
             fullname,
             email,
@@ -40,40 +42,38 @@ export const register = async (req, res) => {
             message: "Account created successfully.",
             success: true
         });
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
-// for login
 export const login = async (req, res) => {
     try {
         const { email, password, role } = req.body;
+        
         if (!email || !password || !role) {
             return res.status(400).json({
                 message: "Something is missing",
                 success: false
             });
         };
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
         if (!user) {
-            res.status(400).json({
-                message: "Incorrect email or password",
+            return res.status(400).json({
+                message: "Incorrect email or password.",
                 success: false,
             })
         }
         const isPasswordMatch = await bcrypt.compare(password, user.password);
         if (!isPasswordMatch) {
-            res.status(400).json({
-                message: "Incorrect email or password",
+            return res.status(400).json({
+                message: "Incorrect email or password.",
                 success: false,
             })
         };
-
-        // check for role is correct or not 
-        if (role != user.role) {
+        // check role is correct or not
+        if (role !== user.role) {
             return res.status(400).json({
-                message: "Account doesn't exist with current role",
+                message: "Account doesn't exist with current role.",
                 success: false
             })
         };
@@ -81,10 +81,8 @@ export const login = async (req, res) => {
         const tokenData = {
             userId: user._id
         }
-
-        // generating token 
-
         const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+
         user = {
             _id: user._id,
             fullname: user.fullname,
@@ -93,45 +91,34 @@ export const login = async (req, res) => {
             role: user.role,
             profile: user.profile
         }
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60, httpsOnly: true, sameSite: 'strict' }).json({
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
         })
-
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
-
-
 export const logout = async (req, res) => {
     try {
         return res.status(200).cookie("token", "", { maxAge: 0 }).json({
-            message: "Logged Out Successfully",
+            message: "Logged out successfully.",
             success: true
         })
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
-
 export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
-        console.log(fullname, email, phoneNumber, bio, skills);
-        // const {fullname} = req.body;
+        
         const file = req.file;
-
-
-        // cloudinary will come here 
+        // cloudinary ayega idhar
         const fileUri = getDataUri(file);
         const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-
-
-
 
 
 
@@ -141,29 +128,28 @@ export const updateProfile = async (req, res) => {
         }
         const userId = req.id; // middleware authentication
         let user = await User.findById(userId);
+
         if (!user) {
             return res.status(400).json({
                 message: "User not found.",
                 success: false
             })
         }
-
-
         // updating data
-
         if(fullname) user.fullname = fullname
         if(email) user.email = email
-        if(phoneNumber) user.phoneNumber = phoneNumber
+        if(phoneNumber)  user.phoneNumber = phoneNumber
         if(bio) user.profile.bio = bio
-        if(skills)  user.profile.skills = skillsArray
-        // resume comes later here 
-
+        if(skills) user.profile.skills = skillsArray
+      
+        // resume comes later here...
         if(cloudResponse){
-            user.profile.resume = cloudResponse.secure_url /// save the cloudinary url
-            user.profile.resumeOriginalName = file.originalname ///save the original filename
+            user.profile.resume = cloudResponse.secure_url // save the cloudinary url
+            user.profile.resumeOriginalName = file.originalname // Save the original file name
         }
-        await user.save();
 
+
+        await user.save();
 
         user = {
             _id: user._id,
@@ -174,14 +160,12 @@ export const updateProfile = async (req, res) => {
             profile: user.profile
         }
 
-
         return res.status(200).json({
             message:"Profile updated successfully.",
             user,
             success:true
         })
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
