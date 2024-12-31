7f5e370911076ed56aa6177270e6a9ebbf434a91
Anant <anantpandey175@gmail.com>
2024-12-31 19:34:11 +0530

    slight change

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -4,7 +4,7 @@ import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import CompaniesTable from './CompaniesTable'
 import { useNavigate } from 'react-router-dom'
-import useGetAllCompanies from '../../hooks/useGetAllCompanies'
+import useGetAllCompanies from '../../hooks/useGetAllCompanies.jsx'
 import { useDispatch } from 'react-redux'
 import { setSearchCompanyByText } from '../../redux/companySlice'
 
